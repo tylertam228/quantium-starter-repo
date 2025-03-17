7e6b6a0852b374ecd9b8bb80d33593d5b89e3f7a
tylertam228 <tyler888.tt@gmail.com>
2025-03-18 00:32:01 +0800

    Removed output.csv and added process_data.py and formatted_data.csv

--- process_data.py
+++ process_data.py
@@ -1,21 +1,42 @@
-import pandas as pd
-import os
+import csv #for read and load .csv file
+import os #command for files
 
-data_folder = "data" #Open File
+DATA_DIRECTORY = "./data" #data file
+OUTPUT_FILE_PATH = "./formatted_data.csv" #the output
 
-csv_files = [f for f in os.listdir(data_folder) if f.endswith(".csv")] #Find all the .csv files
-dataframes = []
-for file in csv_files:
-    file_path = os.path.join(data_folder, file) #Find out the complete path
-    df = pd.read_csv(file_path) #Read File
-    dataframes.append(df) #Adding Elements
+# open the output file
+with open(OUTPUT_FILE_PATH, "w") as output_file: #with open() is for open file and auto close after the process
+    #w mode:write
+    writer = csv.writer(output_file) #writing data
 
-combined_df = pd.concat(dataframes, ignore_index=True) #Combine the data
+    # csv header
+    header = ["sales", "date", "region"]
+    writer.writerow(header) #write csv header
 
-pink_morsel_df = combined_df[combined_df["product"] == "pink morsel"] #Find product == pink morsel
-pink_morsel_df["sales"] = pink_morsel_df["quantity"] * pink_morsel_df["price"] # find the sales of pink morsel
+    # iterate through all files in the data directory
+    for file_name in os.listdir(DATA_DIRECTORY):
+        # open the csv file for reading
+        with open(f"{DATA_DIRECTORY}/{file_name}", "r") as input_file: #read file
+            reader = csv.reader(input_file)
+            # iterate through each row in the csv file
+            row_index = 0
+            for input_row in reader:
+                # if this row is not the csv header, process it
+                if row_index > 0:
+                    # collect data from row
+                    product = input_row[0]
+                    raw_price = input_row[1]
+                    quantity = input_row[2]
+                    transaction_date = input_row[3]
+                    region = input_row[4]
 
-output_df = pink_morsel_df[["sales", "date", "region"]]
-output_df.to_csv("output.csv", index=False)
-#source venv/bin/activate
-print("Done") #run python process_data.py
+                    # if this is a pink morsel transaction, process it
+                    if product == "pink morsel":
+                        # finish formatting data
+                        price = float(raw_price[1:])
+                        sale = price * int(quantity)
+
+                        # write the row to output file
+                        output_row = [sale, transaction_date, region]
+                        writer.writerow(output_row)
+                row_index += 1
